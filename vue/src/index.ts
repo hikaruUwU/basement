a1e5649c7b2d043b5da962663ec2b0f87615171c
UwU <uwu.fionia@gmail.com>
2026-04-08 11:35:07 +0800

    latest

--- vue/src/index.ts
+++ vue/src/index.ts
@@ -3,11 +3,10 @@ import App from './App.vue';
 import './index.css';
 import pinia from './pinia/pinia.ts';
 import router from './router/router.ts';
-import { i18n } from './i18n/i18n.ts';
 import 'element-plus/dist/index.css';
 
 const elementApp = createApp(App);
 
 export const $applicationContext: AppContext = elementApp._context;
 
-elementApp.use(router).use(pinia).use(i18n).mount('#root');
+elementApp.use(router).use(pinia).mount('#root');
