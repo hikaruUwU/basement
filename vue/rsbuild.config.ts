cf382668106009642229e13ef048a81f5453f567
UwU <uwu.fionia@gmail.com>
2026-04-11 20:37:35 +0800

    latest

--- vue/rsbuild.config.ts
+++ vue/rsbuild.config.ts
@@ -1,36 +1,43 @@
-import { defineConfig, type ProxyConfig } from '@rsbuild/core';
-import { pluginVue } from '@rsbuild/plugin-vue';
+import {defineConfig, type ProxyConfig} from '@rsbuild/core';
+import {pluginVue} from '@rsbuild/plugin-vue';
 import AutoImport from 'unplugin-auto-import/rspack';
 import Components from 'unplugin-vue-components/rspack';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
+import {pluginVueJsx} from "@rsbuild/plugin-vue-jsx";
 
 const proxies: ProxyConfig = {
-  '/mock': {
-    target: 'https://jsonplaceholder.typicode.com/posts',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/mock': '',
+    '/mock': {
+        target: 'https://jsonplaceholder.typicode.com/posts',
+        changeOrigin: true,
+        pathRewrite: {
+            '^/mock': '',
+        },
     },
-  },
 };
 
 export default defineConfig({
-  server: {
-    proxy: proxies,
-  },
-  // output: {
-  //   sourceMap: {
-  //     js: 'source-map',
-  //     css: true
-  //   }
-  // },
-  plugins: [
-    pluginVue(),
-    {
-      name: 'flag',
-      setup(api) {
-        api.onBeforeBuild(() => {
-          console.log(`
+    server: {
+        proxy: proxies,
+    },
+    // output: {
+    //   sourceMap: {
+    //     js: 'source-map',
+    //     css: true
+    //   }
+    // },
+    plugins: [
+        pluginVue(),
+        pluginVueJsx({
+            vueJsxOptions: {
+                resolveType: true,
+                enableObjectSlots: true
+            }
+        }),
+        {
+            name: 'flag',
+            setup(api) {
+                api.onBeforeBuild(() => {
+                    console.log(`
               <-.(\`-')  (\`-')  _  (\`-').->(\`-')  _<-. (\`-')   (\`-')  _<-. (\`-')_ (\`-')      
                __( OO)  (OO ).-/  ( OO)_  ( OO).-/   \\(OO )_  ( OO).-/   \\( OO) )( OO).->   
               '-'---.\\  / ,---.  (_)--\\_)(,------.,--./  ,-.)(,------.,--./ ,--/ /    '._   
@@ -41,40 +48,54 @@ export default defineConfig({
               \`------'  \`--' \`--' \`-----' \`------'\`--'   \`--' \`------'\`--'  \`--'    \`--'    
           `);
 
-          console.log('-'.repeat(108));
-        });
-      },
+                    console.log('-'.repeat(108));
+                });
+            },
+        },
+    ],
+    tools: {
+        rspack: {
+            plugins: [
+                AutoImport({
+                    resolvers: [ElementPlusResolver()],
+                }),
+                Components({
+                    resolvers: [ElementPlusResolver()],
+                }),
+            ],
+        },
+        swc:{
+            jsc:{
+                parser:{
+                    syntax: 'typescript',
+                    tsx: true
+                },
+                transform:{
+                    react:{
+                        runtime: "automatic",
+                        importSource: "vue"
+                    }
+                }
+            }
+        }
     },
-  ],
-  tools: {
-    rspack: {
-      plugins: [
-        AutoImport({
-          resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-          resolvers: [ElementPlusResolver()],
-        }),
-      ],
+    performance: {
+        chunkSplit: {
+            strategy: 'split-by-experience',
+        },
+        preload: true,
+        prefetch: {
+            type: 'async-chunks',
+        },
+        buildCache: true,
+        // bundleAnalyze: {
+        //     analyzerMode: 'static',
+        //     reportTitle: () => `PerformanceAnalyzer - ${new Date().toLocaleString()}`,
+        //     defaultSizes: 'gzip',
+        //     openAnalyzer: false,
+        //     excludeAssets: [/\.(png|jpe?g|gif|svg|webp)$/i],
+        //     generateStatsFile: true,
+        //     statsFilename: 'bundle-stats.json',
+        // }
     },
-  },
-  performance: {
-    chunkSplit: {
-      strategy: 'split-by-experience',
-    },
-    preload: true,
-    prefetch: {
-      type: 'async-chunks',
-    },
-    buildCache: true,
-    // bundleAnalyze: {
-    //     analyzerMode: 'static',
-    //     reportTitle: () => `PerformanceAnalyzer - ${new Date().toLocaleString()}`,
-    //     defaultSizes: 'gzip',
-    //     openAnalyzer: false,
-    //     excludeAssets: [/\.(png|jpe?g|gif|svg|webp)$/i],
-    //     generateStatsFile: true,
-    //     statsFilename: 'bundle-stats.json',
-    // }
-  },
 });
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,4 +1,18 @@
 <template>
-  <router-view />
+
+
+  <router-view/>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="tsx">
+
+import {$dialog} from "./service/dialog/dialoging.tsx";
+
+const { close } = $dialog().call(
+    $dialog().preset.plain({
+      content: "Content",
+      onBoth: () => close()
+    })
+)
+
+</script>
