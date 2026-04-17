e645990ff86916d65f76a4d2e26aaa27430ddd33
UwU <uwu.fionia@gmail.com>
2026-04-17 15:09:09 +0800

    latest

--- src/main/java/com/demo/base/service/AuthService.java
+++ src/main/java/com/demo/base/service/AuthService.java
@@ -2,19 +2,16 @@ package com.demo.base.service;
 
 import com.mybatisflex.annotation.UseDataSource;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.ExtensionMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import java.util.StringJoiner;
 
 @Service
-@ExtensionMethod({StringUtils.class})
 @RequiredArgsConstructor
 @UseDataSource("master")
 public class AuthService {
-    {
-        new StringJoiner("");
+
+    public void process() {
+
     }
+
 
 }
