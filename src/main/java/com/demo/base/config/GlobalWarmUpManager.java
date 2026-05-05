e5b69f34acae81a62d0c167b55ed0666f823f31f
UwU <uwu.fionia@gmail.com>
2026-05-05 14:29:59 +0800

    latest

--- src/main/java/com/demo/base/config/GlobalWarmUpManager.java
+++ src/main/java/com/demo/base/config/GlobalWarmUpManager.java
@@ -1,27 +1,31 @@
 package com.demo.base.config;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.concurrent.*;
 
+@Log4j2
 @UtilityClass
 public class GlobalWarmUpManager {
     public static final ExecutorService executor = warmupExecutor();
-
     private static ExecutorService warmupExecutor() {
-        final int nThreads = Runtime.getRuntime().availableProcessors();
-
-        ThreadFactory factory = Thread.ofVirtual()
-                .name("warmup-worker-", 0L)
-                .factory();
-
         return new ThreadPoolExecutor(
                 0,
-                nThreads,
-                16L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(256),
-                factory,
-                new ThreadPoolExecutor.CallerRunsPolicy()
+                Runtime.getRuntime().availableProcessors(),
+                16L, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(64),
+                Thread.ofVirtual().name("warmupWorker-", 0L).factory(),
+                (r, executor) -> Notify.trigger()
         );
+    }
+
+    private class Notify {
+        static {
+            log.warn("PreHeat Task overlimited, some are discarded");
+        }
+
+        public static void trigger() {
+        }
     }
 }
