7f41a79908b09e0ee2b55e0e6e9fb624bef47a10
UwU <uwu.fionia@gmail.com>
2026-05-23 00:47:55 +0800

    latest

--- vue/scripts/collect-dist.ts
+++ vue/scripts/collect-dist.ts
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const rootDir = path.resolve(__dirname, '..');
-const targetRootDir = path.join(rootDir, 'target');
+const targetRootDir = path.join(rootDir, 'dist');
 
 if (fs.existsSync(targetRootDir)) {
     fs.rmSync(targetRootDir, {recursive: true, force: true});
